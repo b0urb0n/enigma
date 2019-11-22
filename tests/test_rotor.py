fc3782fd216c9e0d7d607b2806ea28384a152dc0
Matt Miller <usr.bin.bourbon@gmail.com>
2019-11-22 17:44:26 -0500

    added basic class test

--- tests/test_rotor.py
+++ tests/test_rotor.py
@@ -3,13 +3,20 @@ import pytest
 from enigma import rotor
 
 
+def test_rotor():
+	r = rotor.Rotor(rotor.I)
+	assert r._table == list(rotor.I)
+	
+	with pytest.expectError():
+		rotor.Rotor(rotor.I[:-2])
+
+
 def test_rotor_encode(rotor_I):
 	assert rotor_I.encode('A') == 'H'
 	assert rotor_I.encode('a') == 'Y'
 
 
 def test_rotor_step(rotor_I):
-	assert rotor_I._table == list(rotor.I)
 	rotor_I._step()  # step once - r[0] should now be equal to orig[1]
 	assert rotor_I._table[0] == rotor.I[1]
 	rotor_I._step(25)  # step 25 times - r should now be equal to original table
