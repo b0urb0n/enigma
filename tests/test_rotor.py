c5e37976048dd67ba97c600d056ffa652be8f1fa
Matt Miller <usr.bin.bourbon@gmail.com>
2019-11-22 17:39:36 -0500

    added encode test

--- tests/test_rotor.py
+++ tests/test_rotor.py
@@ -3,6 +3,11 @@ import pytest
 from enigma import rotor
 
 
+def test_rotor_encode(rotor_I):
+	assert rotor_I.encode('A') == 'H'
+	assert rotor_I.encode('a') == 'Y'
+
+
 def test_rotor_step(rotor_I):
 	assert rotor_I._table == list(rotor.I)
 	rotor_I._step()  # step once - r[0] should now be equal to orig[1]
@@ -12,11 +17,11 @@ def test_rotor_step(rotor_I):
 
 
 def test_rotor_callback(rotor_I):
-	flag = False
+	flag = False  # set up a flag to get changed on callback
 	def notch_cb():
 		flag = True
 
-	rotor_I._step(notch_callback=notch_cb)
+	rotor_I._step(notch_callback=notch_cb)  # callback should not get called this step
 	assert flag == False
-	rotor_I._step(notch_callback=notch_cb)
+	rotor_I._step(notch_callback=notch_cb)  # callback should get called this step
 	assert flag == True
