da034889dad99888fed2a3cb4734ddd36a4fd95d
Matt Miller <usr.bin.bourbon@gmail.com>
2019-11-22 20:15:14 -0600

    fixed test_rotor_callback; fixed syntax error

--- tests/test_rotor.py
+++ tests/test_rotor.py
@@ -7,7 +7,7 @@ def test_rotor():
 	r = rotor.Rotor(rotor.I)
 	assert r._table == list(rotor.I)
 	
-	with pytest.expectError():
+	with pytest.raises(ValueError):
 		rotor.Rotor('ABC')
 
 
@@ -18,17 +18,21 @@ def test_rotor_encode(rotor_I):
 
 def test_rotor_step(rotor_I):
 	rotor_I._step()  # step once - r[0] should now be equal to orig[1]
-	assert rotor_I._table[0] == rotor.I[1]
+	assert rotor_I._table[0] == rotor.I[-1]
 	rotor_I._step(25)  # step 25 times - r should now be equal to original table
 	assert rotor_I._table == list(rotor.I)
 
 
 def test_rotor_callback(rotor_I):
+	global flag
 	flag = False  # set up a flag to get changed on callback
 	def notch_cb():
+		global flag
 		flag = True
 
 	rotor_I._step(notch_callback=notch_cb)  # callback should not get called this step
+	print(rotor_I._table[0])
 	assert flag == False
 	rotor_I._step(notch_callback=notch_cb)  # callback should get called this step
+	print(rotor_I._table[0])
 	assert flag == True
